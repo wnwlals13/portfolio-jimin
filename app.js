f4824752611ccec8d561367b1c1eb247e8437020
wnwlals13 <wnwlals13@naver.com>
2021-05-07 11:42:51 +0900

    210507 update some words

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let scrollTop = 0;
 let offset = 0;
 let height = 0;
 let position = 0;
-const skillSet = [50, 40, 60, 50, 50, 50, 40];
+const skillSet = [50, 50, 50, 40, 35, 40, 40];
 const body = document.querySelector("body");
 const mainLogo = document.querySelector(".mainLogo");
 const square = document.querySelector("body .square");
@@ -54,74 +54,73 @@ function contactHandle(data) {
   }
 }
 
-// function fillModal(e) {
-//   const target = e.target.innerText;
-//   modalInner.innerHTML = "";
-//   let modal_title = document.createElement("h2");
-//   let modal_close = document.createElement("p");
-//   modal_close.innerText = "X";
-//   modal_close.className = "modal_close";
-//   modal_close.addEventListener("click", (e) => {
-//     modalWrapper.classList.remove("show");
-//   });
+function fillModal(e) {
+  const target = e.target.innerText;
+  modalInner.innerHTML = "";
+  let modal_title = document.createElement("h2");
+  let modal_close = document.createElement("p");
+  modal_close.innerText = "X";
+  modal_close.className = "modal_close";
+  modal_close.addEventListener("click", (e) => {
+    modalWrapper.classList.remove("show");
+  });
 
-//   let modal_img = document.createElement("img");
-// modal_img.style.width = "100%";
-// modal_img.style.height = "50%";
-// let modal_description = document.createElement("p");
-// modal_description.style.height = "30%";
-// let modal_detail = document.createElement("div");
-// modal_description.className = "modal_desc";
-// modal_detail.className = "modal_detail";
-// modal_detail.innerHTML = `
-// <i class="fab fa-github"></i>
-// <p> source code </p>
-// `;
-// if (target == "Ddoda project") {
-//   modal_title.innerText = target;
-//   modal_title.style.paddingBottom = "10px";
-//   modal_img.src = "./image/ddoda_main.png";
-//   modal_description.innerText = `
-//     프로젝트 기간 : 2020년 11월 10일 ~ 2020년 12월 29일
-//     기술 스택 : Java, Spring framework, JavaScript, jQuery
-//     <구현 기능>
-//     - JavaScript, JQuery, Bootstrap을 이용한 메인 웹 frontend 구현
-//     - 캘린더 API를 활용한 다이어리 CRUD 기능
-//     - 카카오 지도 API 활용한 소모임 장소 정보 제공
-//     - AJAX를 이용한 게시판 댓글과 대댓글 기능 구현
-//   `;
-//   modal_detail.addEventListener("click", (e) => {
-//     window.open("https://github.com/JJINDdoda/JJIN_DDODA_FINAL");
-//   });
-// }
-// else if (target == "Blog project") {
-//   modal_title.innerText = target;
-//   modal_title.style.paddingBottom = "10px";
-//   modal_img.src = "./image/blog_main.png";
-//   modal_description.innerText = `
-//   프로젝트 기간 : 2021년 02월 20일 ~ 2020년 03월
-//   기술 스택 : JavaScript, React hooks, firebase
-//   <구현 기능>
-//   - React hooks 이용해 블로그 CRUD 기능 구현
-//   - firebase auth를 이용한 로그인/아웃
-//   - realtime database를 이용해 디비 연동
-// `;
-//   modal_detail.addEventListener("click", (e) => {
-//     window.open("https://github.com/wnwlals13/myBlogApp");
-//   });
-// }
-//   modalInner.appendChild(modal_title);
-//   modalInner.appendChild(modal_close);
-//   modalInner.appendChild(modal_img);
-//   modalInner.appendChild(modal_description);
-//   modalInner.appendChild(modal_detail);
-// }
+  let modal_img = document.createElement("img");
+  modal_img.style.width = "100%";
+  modal_img.style.height = "50%";
+  let modal_description = document.createElement("p");
+  modal_description.style.height = "30%";
+  let modal_detail = document.createElement("div");
+  modal_description.className = "modal_desc";
+  modal_detail.className = "modal_detail";
+  modal_detail.innerHTML = `
+<i class="fab fa-github"></i>
+<p> source code </p>
+`;
+  if (target == "Ddoda project") {
+    modal_title.innerText = target;
+    modal_title.style.paddingBottom = "10px";
+    modal_img.src = "./image/ddoda_main.png";
+    modal_description.innerText = `
+    프로젝트 기간 : 2020년 11월 10일 ~ 2020년 12월 29일
+    기술 스택 : Java, Spring framework, JavaScript, jQuery
+    <구현 기능>
+    - JavaScript, JQuery, Bootstrap을 이용한 메인 웹 frontend 구현
+    - 캘린더 API를 활용한 다이어리 CRUD 기능
+    - 카카오 지도 API 활용한 소모임 장소 정보 제공
+    - AJAX를 이용한 게시판 댓글과 대댓글 기능 구현
+  `;
+    modal_detail.addEventListener("click", (e) => {
+      window.open("https://github.com/JJINDdoda/JJIN_DDODA_FINAL");
+    });
+  } else if (target == "Blog project") {
+    modal_title.innerText = target;
+    modal_title.style.paddingBottom = "10px";
+    modal_img.src = "./image/blog_main.png";
+    modal_description.innerText = `
+  프로젝트 기간 : 2021년 02월 20일 ~ 2020년 03월
+  기술 스택 : JavaScript, React hooks, firebase
+  <구현 기능>
+  - React hooks 이용해 블로그 CRUD 기능 구현
+  - firebase auth를 이용한 로그인/아웃
+  - realtime database를 이용해 디비 연동
+`;
+    modal_detail.addEventListener("click", (e) => {
+      window.open("https://github.com/wnwlals13/myBlogApp");
+    });
+  }
+  modalInner.appendChild(modal_title);
+  modalInner.appendChild(modal_close);
+  modalInner.appendChild(modal_img);
+  modalInner.appendChild(modal_description);
+  modalInner.appendChild(modal_detail);
+}
 
-// function offModal(e) {
-//   if (e.target.className == "modalWrapper show") {
-//     modalWrapper.classList.remove("show");
-//   }
-// }
+function offModal(e) {
+  if (e.target.className == "modalWrapper show") {
+    modalWrapper.classList.remove("show");
+  }
+}
 mainLogo.addEventListener("click", () => {
   location.href = location.href;
 });
@@ -145,25 +144,25 @@ goUp.addEventListener("click", (e) => {
   window.scrollTo({ top: landingPage.offsetTop, behavior: "smooth" });
 });
 
-// projects.addEventListener("click", (e) => {
-//   fillModal(e);
-//   modalWrapper.style.height = `${body.clientHeight}px`;
-//   modalWrapper.style.top = "0";
-//   modalWrapper.classList.add("show");
-//   modalInner.classList.add("show");
-//   console.log(e.target.innerText);
-// });
 projects.addEventListener("click", (e) => {
-  if (e.target.className == "project") {
-    window.open(
-      "https://drive.google.com/file/d/1o4mWh5w93CdQWIDWE16SkL3M0TtmUwJC/view?usp=sharing"
-    );
-  } else if (e.target.className == "project second") {
-    window.open(
-      "https://drive.google.com/file/d/1ApR_EH8FkSMz0ubsyrflyK3XrYeDb4sP/view?usp=sharing"
-    );
-  }
+  fillModal(e);
+  modalWrapper.style.height = `${body.clientHeight}px`;
+  modalWrapper.style.top = "0";
+  modalWrapper.classList.add("show");
+  modalInner.classList.add("show");
+  console.log(e.target.innerText);
 });
+// projects.addEventListener("click", (e) => {
+//   if (e.target.className == "project") {
+//     window.open(
+//       "https://drive.google.com/file/d/1o4mWh5w93CdQWIDWE16SkL3M0TtmUwJC/view?usp=sharing"
+//     );
+//   } else if (e.target.className == "project second") {
+//     window.open(
+//       "https://drive.google.com/file/d/1ApR_EH8FkSMz0ubsyrflyK3XrYeDb4sP/view?usp=sharing"
+//     );
+//   }
+// });
 
 modalWrapper.addEventListener("click", (e) => {
   offModal(e);
